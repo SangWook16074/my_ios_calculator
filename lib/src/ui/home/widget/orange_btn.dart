c66bc6dec01d4ce4c9a0bdd0cf328e4e8389714d
HanSangWook <qazws78941@naver.com>
2023-02-05 22:35:25 +0900

    오렌지 색 버튼 작업 전 이전 Stack 위젯 삭제

--- lib/src/ui/home/widget/orange_btn.dart
+++ lib/src/ui/home/widget/orange_btn.dart
@@ -1,8 +1,11 @@
-// 2023 - 02 - 01 오전 01:31 작성자 : 한상욱
+// 생성일 : 2023년 02월 01일 오전 01:31
+// 작성자 : 한상욱
+// 수정일 : 2023년 02월 05일 오후 10:34
+// 수정자 : 한상욱
+
 // 사측연산을 하는 오렌지색 버튼 클래스
 
 import 'package:flutter/material.dart';
-import 'package:flutter_ios_calculator/src/ui/home/widget/back_ground_btn.dart';
 import 'package:flutter_ios_calculator/src/ui/home/widget/basic_btn.dart';
 import 'package:flutter_ios_calculator/src/ui/home/widget/btn_color.dart';
 
@@ -12,11 +15,6 @@ class OrangeBtn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        BackGroundBtn(),
-        BasicBtn(color: BtnColor.orange, child: icon),
-      ],
-    );
+    return BasicBtn(color: BtnColor.orange, child: icon);
   }
 }
