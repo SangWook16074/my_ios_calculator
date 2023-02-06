488e313e117babe58e0911efe3931d6b12c10653
HanSangWook <qazws78941@gmail.com>
2023-02-07 02:18:43 +0900

    2023년 2월 7일 아이폰 계산기 클론 프로젝트 작업
    1. 오렌지색버튼 애니메이션 효과 작업을 위한 새 branch 게시

--- lib/src/controller/button_controller.dart
+++ lib/src/controller/button_controller.dart
@@ -2,7 +2,7 @@ import 'package:get/get.dart';
 
 // 생성일 : 2023년 02월 05일 오후 10:52
 // 작성자 : 한상욱
-// 최종 수정일 : 2023년 02월 05일 오후 10:52
+// 최종 수정일 : 2023년 02월 07일 오전 02:13
 // 최종 수정자 : 한상욱
 
 class ButtonController extends GetxController
